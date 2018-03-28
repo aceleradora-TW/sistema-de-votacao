951b9f4173e750f7cfba2302dcfb1c332ed8787f
othman853 <anuar.yasser@gmail.com>
2018-03-28 19:13:50 -0300

    Configure Vue and SFC

---
+++ src/view/components/main.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+      <h1>Projetos</h1>
+      <ul>
+          <li v-for="project in projects">{{project.name}}</li>
+      </ul>
+  </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                projects: [
+                    {
+                        name: 'Projeto 01'
+                    }
+                ]
+            }
+        }
+    }
+</script>
