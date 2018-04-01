c5bac99d5ab596a9a873ddff6f85267acb6f3ada
othman853 <anuar.yasser@gmail.com>
2018-04-01 19:58:30 -0300

    Adds cypress

---
+++ cypress/integration/project-list.test.js
@@ -0,0 +1,13 @@
+describe('Project List', () => {
+  it('loads successfully', () => {
+      cy.visit('/')
+  })
+
+  it('has a title', () => {
+      cy.get('h1').should('contain', 'Projetos')
+  })
+
+  it('has the hardcoded project', () => {
+      cy.get('ul > li:first').should('contain', 'Projeto 01')
+  })
+})
