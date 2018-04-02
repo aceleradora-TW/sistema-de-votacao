8d6995a2140883045ae7a6e5a75aa19e149b1864
othman853 <anuar.yasser@gmail.com>
2018-04-02 19:58:23 -0300

    #10 @othman853: mongo URI via env vars

--- src/database.js
+++ src/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 module.exports = {
-  connect: () => mongoose.connect('mongodb://127.0.0.1/doot'),
+  connect: () => mongoose.connect(process.env.MONGODB_URI),
 
   disconnect: () => new Promise((resolve, reject) =>
     mongoose.connection.close((error) => error
