1802a178dd5f57d74fc184cb5ecf4c4a13db2104
Ghilherme Ghidorsi <ghidorsi@thoughtworks.com>
2018-04-03 14:36:37 -0300

    Escrever uma file por dia de log

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
+<<<<<<< HEAD
 const server = require("./src/server");
 const { port } = require("./settings");
 
 server().listen(port, () => console.log("Up on port", port));
+=======
+const server = require('./server');
+
+const PORT = process.env.PORT || (3000);
+
+server()
+  .listen(PORT, () => console.log('Up on port', PORT));
+>>>>>>> Escrever uma file por dia de log
