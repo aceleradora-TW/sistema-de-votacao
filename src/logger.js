c12fb059ebfc584e09dbf4170ed9a0f2ad016204
Ghilherme Ghidorsi <ghidorsi@thoughtworks.com>
2018-04-03 18:23:36 -0300

    Single quotes

--- src/logger.js
+++ src/logger.js
@@ -1,5 +1,5 @@
-const winston = require("winston")
-const expressWinston = require("express-winston")
+const winston = require('winston')
+const expressWinston = require('express-winston')
 
 const loggerOptions = {
   transports: [
--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const { join } = require('path')
-const { winstonMiddleware } = require("./logger")
+const { winstonMiddleware } = require('./logger')
 
 module.exports = () =>
   express()
     .use(winstonMiddleware)
-    .use(express.static(join(__dirname, "public")))
+    .use(express.static(join(__dirname, 'public')))
