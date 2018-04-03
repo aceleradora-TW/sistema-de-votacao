4ea411f1146594c6d1bc25f28fe44e44421214eb
Ghilherme Ghidorsi <ghidorsi@thoughtworks.com>
2018-04-03 18:10:18 -0300

    Mudando de console log pra winston

--- bin/cypress-run.js
+++ bin/cypress-run.js
@@ -1,12 +1,13 @@
 const cypress = require('cypress')
 const server = require('./test-server')
+const { logger } = require('./../src/logger')
 
 server()
   .then((stopServer) =>
     cypress
       .run()
       .then(stopServer)
-      .catch(error => console.log(error.message))
+      .catch(error => logger.info(error.message))
   )
-  .catch(error => console.log(error.message))
+  .catch(error => logger.info(error.message))
 
--- bin/test-server.js
+++ bin/test-server.js
@@ -1,13 +1,14 @@
 const regularServer = require('../src/server')
+const { logger } = require('./../src/logger')
 
 const stop = (server) => () => {
   server.close()
-  console.log('Test server stopped')
+  logger.info('Test server stopped')
 }
 
 const start = () => new Promise((resolve, reject) => {
   const server = regularServer().listen(3000, () => {
-    console.log('Test server started at ', 3000)
+    logger.info("Test server started at ", 3000);
 
     return resolve(stop(server))
   })
