fd392129a7b840a49f742d544fe421b69158cc29
othman853 <anuar.yasser@gmail.com>
2018-04-04 16:17:07 -0300

    #1 @othman853: Adds the Projects model

---
+++ test/unit/models/project.test.js
@@ -0,0 +1,12 @@
+const Project = require('models/project')
+
+describe('A project model', () => {
+  it('is called Project', () => {
+    expect(Project.modelName).toEqual('Project')
+  })
+
+  it('has an attribute of type String called "name"', () => {
+    expect(Project.schema.paths.name).toBeTruthy()
+    expect(Project.schema.paths.name.instance).toEqual('String')
+  })
+})
