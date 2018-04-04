3436ba41fa2147ca6a505704ddd5a4bc3e9b36ea
othman853 <anuar.yasser@gmail.com>
2018-04-04 15:03:10 -0300

    #19 @othman853: Adds jest and cypress plugins for eslint

--- cypress/integration/project-list.test.js
+++ cypress/integration/project-list.test.js
@@ -1,13 +1,13 @@
 describe('Project List', () => {
   it('loads successfully', () => {
-      cy.visit('/')
+    cy.visit('/')
   })
 
   it('has a title', () => {
-      cy.get('h1').should('contain', 'Projetos')
+    cy.get('h1').should('contain', 'Projetos')
   })
 
   it('has the hardcoded project', () => {
-      cy.get('ul > li:first').should('contain', 'Projeto 01')
+    cy.get('ul > li:first').should('contain', 'Projeto 01')
   })
 })
