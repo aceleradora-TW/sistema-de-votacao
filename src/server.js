0597baa028414c112cbc179f3958870fd46736e5
Ghilherme Ghidorsi <ghidorsi@thoughtworks.com>
2018-04-10 14:01:09 -0300

    Adicionando rota de projetos no server

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const projectRoutes = require('./routes/project')
 const { join } = require('path')
 const { winstonMiddleware } = require('./logger')
 
 module.exports = () =>
   express()
     .use(winstonMiddleware)
+    .use('/projects', projectRoutes)
     .use(express.static(join(__dirname, 'public')))
