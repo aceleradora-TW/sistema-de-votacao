eed6e052ce0ac20a10d371cb75a0a77f899170d5
Ghilherme Ghidorsi <ghidorsi@thoughtworks.com>
2018-04-10 13:59:40 -0300

    Conectar no mongoose ao iniciar o servidor

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const server = require('./src/server')
-const { port } = require('./settings')
+const { port, databaseUri } = require('./settings')
 const { logger } = require('./src/logger')
+const database = require('./src/database')
+
+database(databaseUri).connect()
 
 server()
   .listen(port, () => logger.info('Up on port', port))
