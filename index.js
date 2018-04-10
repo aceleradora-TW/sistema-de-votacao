c908d7baa4cb36f4651e249fcf35af45e711f53f
Ghilherme Ghidorsi <ghidorsi@thoughtworks.com>
2018-04-10 18:54:46 -0300

    Usando promises pra abrir o servidor com o db

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const { port, databaseUri } = require('./settings')
 const { logger } = require('./src/logger')
 const database = require('./src/database')
 
-database(databaseUri).connect()
-
-server()
-  .listen(port, () => logger.info('Up on port', port))
+database(databaseUri)
+  .connect()
+  .then(server)
+  .then(createdServer => createdServer.listen(port, () => logger.info('Up on port', port)))
+  .catch(logger.error)
