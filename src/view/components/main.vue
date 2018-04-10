0a1420df5b3b6a9825e4b43018a25e923569780d
Ghilherme Ghidorsi <ghidorsi@thoughtworks.com>
2018-04-10 14:01:21 -0300

    Consertando lint e recebendo projetos por request

--- src/view/components/main.vue
+++ src/view/components/main.vue
@@ -2,21 +2,24 @@
   <div>
       <h1>Projetos</h1>
       <ul>
-          <li v-for="project in projects">{{project.name}}</li>
+          <li v-for="project in projects" v-bind:key="project">{{project.name}}</li>
       </ul>
   </div>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                projects: [
-                    {
-                        name: 'Projeto 01'
-                    }
-                ]
-            }
+export default {
+    data() {
+        return {
+            projects: []
         }
+    },
+
+    created() {
+        this.$http.get('/projects').then(response => {
+            this.projects = JSON.parse(response.bodyText)
+        }).catch(err => console.log(err))
     }
+}
 </script>
+
--- src/view/index.js
+++ src/view/index.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
+import VueResource from 'vue-resource'
 import Main from './components/main.vue'
+
+Vue.use(VueResource)
 
 // eslint-disable-next-line no-unused-vars
 const application = new Vue({
