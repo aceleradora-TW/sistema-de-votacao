e6d8edd53cac0f37bd8d6765a79c52c461549fdb
othman853 <anuar.yasser@gmail.com>
2018-04-17 15:38:22 -0300

    #1 @othman853: Functional tests based on DB seed

--- bin/db-seed.js
+++ bin/db-seed.js
@@ -5,13 +5,25 @@ const Project = require('../src/models/project')
 
 const db = database(settings.databaseUri)
 
+const CLEANUP = 'Cleaning up Project documents...'
+const INSERT = 'Inserting new Project documents...'
+const DONE = 'All done!'
+const DISCONNECT = 'Disconnecting from the DB...'
+const START = 'Connected to the DB. Seeding will now start.'
+
+const logAndThen = (message, action = () => {}) => () => {
+  logger.info(message)
+  action()
+}
+
 db
   .connect()
-  .then(() =>
-    Project.collection.insert([
-      { name: 'Projeto 01' },
-      { name: 'Projeto 02' },
-      { name: 'Projeto 03' },
-    ]))
-  .then(db.disconnect)
+  .then(logAndThen(START))
+  .then(logAndThen(CLEANUP, () => Project.remove({}).exec()))
+  .then(logAndThen(INSERT, () => Project.collection.insert([
+    { name: 'Projeto 01' },
+    { name: 'Projeto 02' },
+    { name: 'Projeto 03' }])))
+  .then(logAndThen(DISCONNECT, db.disconnect))
+  .then(logAndThen(DONE))
   .catch(logger.error)
--- cypress/integration/project-list.test.js
+++ cypress/integration/project-list.test.js
@@ -1,3 +1,5 @@
+const projectListItem = n => cy.get('ul').children(n)
+
 describe('Project List', () => {
   it('loads successfully', () => {
     cy.visit('/')
@@ -5,5 +7,17 @@ describe('Project List', () => {
 
   it('has a title', () => {
     cy.get('h1').should('contain', 'Projetos')
+  })
+
+  it('has "Projeto 01" in the list', () => {
+    projectListItem(0).should('contain', 'Projeto 01')
+  })
+
+  it('has "Projeto 02" in the list', () => {
+    projectListItem(1).should('contain', 'Projeto 02')
+  })
+
+  it('has "Projeto 03" in the list', () => {
+    projectListItem(2).should('contain', 'Projeto 03')
   })
 })
