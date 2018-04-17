dcef791ac8f5e338014bc24b848ee3f99b0ca798
othman853 <anuar.yasser@gmail.com>
2018-04-17 15:56:14 -0300

    #1 @othman853: Better name for project selector shortcut

--- cypress/integration/project-list.test.js
+++ cypress/integration/project-list.test.js
@@ -1,4 +1,4 @@
-const projectListItem = n => cy.get('ul').children(n)
+const projectAtPosition = n => cy.get('ul').children(n)
 
 describe('Project List', () => {
   it('loads successfully', () => {
@@ -10,14 +10,14 @@ describe('Project List', () => {
   })
 
   it('has "Projeto 01" in the list', () => {
-    projectListItem(0).should('contain', 'Projeto 01')
+    projectAtPosition(0).should('contain', 'Projeto 01')
   })
 
   it('has "Projeto 02" in the list', () => {
-    projectListItem(1).should('contain', 'Projeto 02')
+    projectAtPosition(1).should('contain', 'Projeto 02')
   })
 
   it('has "Projeto 03" in the list', () => {
-    projectListItem(2).should('contain', 'Projeto 03')
+    projectAtPosition(2).should('contain', 'Projeto 03')
   })
 })
